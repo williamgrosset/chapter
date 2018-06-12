4d6dc8ab05b911418b92f70209109f33b9ab604e
William Grosset <williamhgrosset@gmail.com>
2018-06-11 17:33:30 -0700

    Update makefile (add run); get current .git dir

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 .phony setup:
 setup: main.cpp
-	g++ -I. main.cpp -std=c++11 -o chapter.o
+		g++ -I. main.cpp -std=c++11 -o chapter.o
+
+run: chapter.o
+		./chapter.o
 
 .phony clean:
-	rm -rf *.o
+		rm -rf *.o
--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
+#include <unistd.h>
 #include <jamspell/spell_corrector.hpp>
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    char buffer[255];
+    char* answer = getcwd(buffer, sizeof(buffer));
+    string s_cwd;
+
+    if (answer) {
+        s_cwd = answer;
+    }
+    printf("%s/.git\n", s_cwd.c_str());
+
     // Grab current directory and search .git folder
     // Identify branch and last commit NOT on remote
     // Print out text from commit message
