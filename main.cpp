957e222f50103a620fbbbebc1ea7930705ef7f00
William Grosset <williamhgrosset@gmail.com>
2018-06-11 18:37:53 -0700

    Print out content of COMMIT_EDITMSG

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
+
+#include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <jamspell/spell_corrector.hpp>
 using namespace std;
@@ -14,11 +17,19 @@ int main(int argc, char* argv[]) {
     if (answer) {
         s_cwd = answer;
     }
-    printf("%s/.git\n", s_cwd.c_str());
+    printf("%s/.git/COMMIT_EDITMSG\n", s_cwd.c_str());
 
     // Grab current directory and search .git folder
     // Identify branch and last commit NOT on remote
     // Print out text from commit message
+
+    string commit_msg = s_cwd + "./git/COMMIT_EDITMSG";
+    ifstream f(commit_msg.c_str());
+
+    if (f.is_open()) {
+      cout << f.rdbuf();
+    }
+
     // TODO: AUDIT
     return 0;
 }
