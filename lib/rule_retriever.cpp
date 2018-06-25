1362338506779b2972cefca72672599b0322cd8a
William Grosset <williamhgrosset@gmail.com>
2018-06-25 16:00:51 -0700

    Nit: Fix identation

--- lib/rule_retriever.cpp
+++ lib/rule_retriever.cpp
@@ -16,70 +16,70 @@ std::ifstream readConfigFile() {
 
     if (cwd) {
         std::string s_cwd(cwd);
-        std::ifstream i(s_cwd + "/COMMIT_MSG_RULES.json");
-        return i;
+        std::ifstream f(s_cwd + "/COMMIT_MSG_RULES.json");
+        return f;
     } else {
-        std::ifstream i("/COMMIT_MSG_RULES.json");
-        return i;
+        std::ifstream f("/COMMIT_MSG_RULES.json");
+        return f;
     }
 }
 
 nlohmann::json convertFileToJson() {
-  std::ifstream i = readConfigFile(); 
-  nlohmann::json j;
-  i >> j;
+    std::ifstream f = readConfigFile(); 
+    nlohmann::json j;
+    f >> j;
 
-  return j;
+    return j;
 }
 
 int getSummaryMaxLength() {
-  nlohmann::json j = convertFileToJson();
-  // if (isInt(j["sum_max_len"]))
-  // if (j["sum_max_len"] > getSummaryMinLen())
-  return j["sum_max_len"];
+    nlohmann::json j = convertFileToJson();
+    // if (isInt(j["sum_max_len"]))
+    // if (j["sum_max_len"] > getSummaryMinLen())
+    return j["sum_max_len"];
 }
 
 int getSummaryMinLength() {
-  nlohmann::json j = convertFileToJson();
-  return j["sum_min_len"];
+    nlohmann::json j = convertFileToJson();
+    return j["sum_min_len"];
 }
 
 bool requiresDescription() {
-  nlohmann::json j = convertFileToJson();
-  return j["desc"]["required"];
+    nlohmann::json j = convertFileToJson();
+    return j["desc"]["required"];
 }
 
 int getDescriptionMaxLength() {
-  nlohmann::json j = convertFileToJson();
-  return j["desc"]["max_len"];
+    nlohmann::json j = convertFileToJson();
+    return j["desc"]["max_len"];
 }
 
 int getDescriptionMinLength() {
-  nlohmann::json j = convertFileToJson();
-  return j["desc"]["min_len"];
+    nlohmann::json j = convertFileToJson();
+    return j["desc"]["min_len"];
 }
 
 int getBulletPoints() {
-  nlohmann::json j = convertFileToJson();
-  return j["bullet_points"];
+    nlohmann::json j = convertFileToJson();
+    return j["bullet_points"];
 }
 
 bool allowsTypos() {
-  nlohmann::json j = convertFileToJson();
-  return j["allows_typos"];
+    nlohmann::json j = convertFileToJson();
+    return j["allows_typos"];
 }
 
 bool requiresSummaryCapital() {
-  nlohmann::json j = convertFileToJson();
-  return j["sum_capital"];
+    nlohmann::json j = convertFileToJson();
+    return j["sum_capital"];
 }
 
 bool requiresNitFormat() {
-  nlohmann::json j = convertFileToJson();
-  return j["nit_format"];
+    nlohmann::json j = convertFileToJson();
+    return j["nit_format"];
 }
 
 bool requiresWIPFormat() {
-  nlohmann::json j = convertFileToJson();
-  return j["wip_format"];
+    nlohmann::json j = convertFileToJson();
+    return j["wip_format"];
 }
