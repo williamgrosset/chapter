7f92b45650300dceb19e2cd2804854513d9ebd91
William Grosset <williamhgrosset@gmail.com>
2018-06-25 23:36:06 -0700

    Use std::cout for printing and pass values into statements

--- lib/console_printer.cpp
+++ lib/console_printer.cpp
@@ -22,53 +22,58 @@ void displayAuditResults(nlohmann::json rulesJSON, const std::string commit_msg)
     // printf("%s\n", commit_msg_mod.c_str());
 
     if (requiresSummaryCapital(rulesJSON) && isFirstLetterCapitalized(commit_msg)) {
-        printf("\U00002705 Success: First letter of summary must be capitalized. \n");
+        std::cout << "\U00002705 Success: First letter of summary must be capitalized." << "\n";
     } else {
-        printf("\U0000274C Error: First letter of summary must be capitalized. \n");
+        std::cout << "\U0000274C Error: First letter of summary must be capitalized." << "\n";
     }
 
     if (requiresNitFormat(rulesJSON) && !containsCorrectNitFormat(commit_msg)) {
-        printf("\U0000274C Error: \"Nit:\" commits must have the correct format. \n");
+        std::cout << "\U0000274C Error: \"Nit:\" commits must have the correct format." << "\n";
     }
 
     if (requiresWIPFormat(rulesJSON) && !containsCorrectWIPFormat(commit_msg)) {
-        printf("\U0000274C Error: \"WIP:\" commits must have the correct format. \n");
+        std::cout << "\U0000274C Error: \"WIP:\" commits must have the correct format." << "\n";
     }
 
-    if (isSummaryMinLength(commit_msg, getSummaryMinLength(rulesJSON))) {
-        printf("\U0000274C Error: Summary must be above 18 characters. \n");
+    int summaryMinLength = getSummaryMinLength(rulesJSON);
+    if (isSummaryMinLength(commit_msg, summaryMinLength)) {
+        std::cout << "\U0000274C Error: Summary must be above " << summaryMinLength << " characters.\n";
     } else {
-        printf("\U00002705 Success: Summary must be above 18 characters. \n");
+        std::cout << "\U00002705 Success: Summary must be above " << summaryMinLength << " characters.\n";
     }
 
-    if (isSummaryMaxLength(commit_msg, getSummaryMaxLength(rulesJSON))) {
-        printf("\U0000274C Error: Summary must not exceed 50 characters. \n");
+    int summaryMaxLength = getSummaryMaxLength(rulesJSON);
+    if (isSummaryMaxLength(commit_msg, summaryMaxLength)) {
+        std::cout << "\U0000274C Error: Summary must not exceed " << summaryMaxLength << " characters.\n";
     } else {
-        printf("\U00002705 Success: Summary must not exceed 50 characters. \n");
+        std::cout << "\U00002705 Success: Summary must not exceed " << summaryMaxLength << " characters.\n";
     }
 
     if (requiresDescription(rulesJSON) && containsDescription(commit_msg)) {
         printf("\U00002705 Success: Description is required. \n");
+        int descMinLength = getDescriptionMinLength(rulesJSON);
+        int descMaxLength = getDescriptionMaxLength(rulesJSON);
 
-        if (isDescriptionMinLength(commit_msg, getDescriptionMinLength(rulesJSON))) {
-            printf("\U0000274C Error: Description must be above 40 characters. \n");
+        if (isDescriptionMinLength(commit_msg, descMinLength)) {
+            std::cout << "\U0000274C Error: Description must be above " << descMinLength << " characters.\n";
         } else {
-            printf("\U00002705 Success: Description must be above 40 characters. \n");
+            std::cout << "\U00002705 Success: Description must be above " << descMinLength << " characters.\n";
         }
 
-        if (isDescriptionMaxLength(commit_msg, getDescriptionMaxLength(rulesJSON))) {
-            printf("\U0000274C Error: Description must not exceed 72 characters. \n");
+        if (isDescriptionMaxLength(commit_msg, descMaxLength)) {
+            std::cout << "\U0000274C Error: Description must not exceed " << descMaxLength << " characters.\n";
         } else {
-            printf("\U00002705 Success: Description must not exceed 72 characters. \n");
+            std::cout << "\U00002705 Success: Description must not exceed " << descMaxLength << " characters.\n";
         }
     } else {
         printf("\U0000274C Error: Description is required. \n");
     }
 
-    if (containsBulletPoints(commit_msg, getBulletPoints(rulesJSON))) {
-        printf("\U00002705 Success: 3 bullet points are required. \n");
+    int bulletPointsVal = getBulletPoints(rulesJSON);
+    if (containsBulletPoints(commit_msg, bulletPointsVal)) {
+        std::cout << "\U00002705 Success: " << bulletPointsVal << " bullet points are required. \n";
     } else {
-        printf("\U0000274C Error: 3 bullet points are required. \n");
+        std::cout << "\U0000274C Error: " << bulletPointsVal << " bullet points are required. \n";
     }
 
     printf("************        \U00002699 End of audit.        ************\n");
