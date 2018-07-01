682d6332c5d6cb86a8d98bb6c9793c606c0875d1
William Grosset <williamhgrosset@gmail.com>
2018-06-30 21:07:28 -0700

    WIP: Remove comments when auditing commit message

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <boost/regex.hpp>
 #include <nlohmann/json.hpp>
 #include <lib/rule_retriever.hpp>
 #include <lib/console_printer.hpp>
@@ -13,7 +14,7 @@ std::ifstream readGitCommitMsgFile() {
     try {
         if (cwd) {
             std::string s_cwd(cwd);
-            std::ifstream f(s_cwd + "/.git/COMMIT_EDITMSG");
+            std::ifstream f(s_cwd + "/test.txt");
             return f;
         } else {
             throw std::exception();
@@ -24,9 +25,28 @@ std::ifstream readGitCommitMsgFile() {
     }
 }
 
+// TODO: Also remove '\n' characters
+void removeComments(std::string& msg) {
+    for (int i = 0; i < msg.length(); i++) {
+        if (i != 0) {
+            if (msg[i - 1] == '\n' && msg[i] == '#') {
+                int j = i;
+
+                // Look for position of next "\n"
+                while (msg[j] != '\n') {
+                    j++;
+                }
+
+                msg.replace(i, j - i, "");
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     std::ifstream f = readGitCommitMsgFile();
     std::ostringstream s_stream;
+    boost::smatch comments;
 
     if (f.is_open()) {
         s_stream << f.rdbuf();
@@ -35,6 +55,8 @@ int main(int argc, char* argv[]) {
     std::string commitMsg = s_stream.str();
     nlohmann::json rulesJSON = convertFileToJSON();
 
+    removeComments(commitMsg);
+    std::cout << "RESULT: " << commitMsg << "\n";
     displayAuditResults(rulesJSON, commitMsg);
 
     return 0;
